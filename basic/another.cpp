88441391feba1d1408f698d1b117d26d473563e4
YangFan <mr_younghusband@yeah.net>
2024-09-18 20:08:32 +0800

    0918

--- basic/another.cpp
+++ basic/another.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
@@ -30,22 +31,22 @@ using namespace std;
 //     // 找到第三小的元素
 //     nth_element(nums.begin(), nums.begin() + 2, nums.end());
 //     cout << endl << "The third smallest element is: " << nums[2] << endl;
-//
 //     cout << "Partial sorted range: ";
 //     for(int num: nums) {
 //         cout << num << " ";
 //     }cout << endl;
 //     // 1 2 3 4 5 6  n = 6 k = 3  x =
-
-// char s[100]={0};
-// sprintf(s, "%d", 123); //十进制输出产生"123"
-// sprintf(s, "%4d%4d", 123, 4567); //指定宽度不足的左边补空格，产生：" 1234567"
-// sprintf(s, "%8o", 123);	//八进制输出，宽度占8个位置
-// sprintf(s, "%8x", 4567); //小写16 进制，宽度占8 个位置，右对齐
-// sprintf(s, "%10.3f", 3.1415626); //产生：" 3.142"
-// int i = 100;
-// sprintf(s, "%.2f", i);	//注意这是不对的
-
+//
+//     char s[100]={0};
+//     sprintf(s, "%d", 123); //十进制输出产生"123"
+//     sprintf(s, "%4d%4d", 123, 4567); //指定宽度不足的左边补空格，产生：" 1234567"
+//     sprintf(s, "%8o", 123);	//八进制输出，宽度占8个位置
+//     sprintf(s, "%8x", 4567); //小写16 进制，宽度占8 个位置，右对齐
+//     sprintf(s, "%10.3f", 3.1415626); //产生：" 3.142"
+//     // cout << s << endl;
+//     int i = 100;
+//     sprintf(s, "%.2f", (float)i);	//注意这是不对的
+//
 //
 // }
 
---
+++ huawei/done/binary_max_len.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Younghusband on 2024/9/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+
+    string toBi(long num) {
+        if(num == 0) return "0";
+        string biStr = "";
+        while(num > 0) {
+            biStr = (num % 2 == 0 ? "0" : "1") + biStr;  // 逆序相加
+            num = num >> 1;
+        }
+        return biStr;
+    }
+
+
+public:
+    // 待实现函数，在此函数中填入答题代码
+    int GetLongestNum(long num)
+    {
+        string biStr = toBi(num);
+        if(biStr.size() == 1) return 1;
+        int left = 0;
+        int right = left + 1;
+        int maxlen = 1;
+        while(right <= biStr.size() - 1) {
+            if(biStr[right] == biStr[left]) {
+                maxlen = max(maxlen, right - left + 1);
+                right++;
+            } else {
+                left++;
+            }
+        }
+        return maxlen;
+    }
+};
+
+
+
+
+
+// int main() {
+//
+//     long num;
+//     cin >> num;
+//     Solution slo;
+//     cout << slo.GetLongestNum(num);
+//
+// }
